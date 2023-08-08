c67315216d223e3004cb3aa55dfdeea1ea01b967
EvgeniaRogotneva <evgeniya.rogotneva@revelsystems.com>
2023-08-08 18:32:52 +0200

    StartPoint class added

--- src/left_menu/StartPointCoordInput.js
+++ src/left_menu/StartPointCoordInput.js
@@ -1,9 +1,25 @@
 const StartPointCoordInput = ({ max, min, valueX, valueY }) => {
   return (
     <>
-      X: <input type="number" label="X" min={min} max={max} value={valueX} />
-      Y: <input type="number" label="Y" min={min} max={max} value={valueY} />
-      <button>Ok</button>
+      X:{" "}
+      <input
+        className="start-point-coord-input-x"
+        type="number"
+        label="X"
+        min={min}
+        max={max}
+        value={valueX}
+      />
+      Y:{" "}
+      <input
+        className="start-point-coord-input-y"
+        type="number"
+        label="Y"
+        min={min}
+        max={max}
+        value={valueY}
+      />
+      <button className="submit-coord">Ok</button>
     </>
   );
 };
