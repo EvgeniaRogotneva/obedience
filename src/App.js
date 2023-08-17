24a945ac9f5546a49bee9e4906c73c07b98c868f
EvgeniaRogotneva <evgeniya.rogotneva@revelsystems.com>
2023-08-17 17:59:47 +0200

    remove props from app file

--- src/App.js
+++ src/App.js
@@ -1,20 +1,15 @@
-import { useContext } from "react";
 import "./App.css";
 import Field from "./field/Field";
 import LeftMenu from "./left_menu/LeftMenu";
 
-import { Context } from "./state/Context";
-
 function App() {
-  const value = useContext(Context);
-  const [state] = value;
   return (
     <div className="App">
       <h1 className="DogObedienceStewartToolHeader">
         Dog Obedience Stewart Tool
       </h1>
       <LeftMenu />
-      <Field x={state.field.coord["valueX"]} y={state.field.coord["valueY"]} />
+      <Field />
 
       <p> !!!! площадь под конусами - 0,4-0,5 кв м</p>
       <p>добавить нормальные стили на всю страницу</p>
