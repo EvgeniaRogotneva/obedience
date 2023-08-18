7bbffd37c49221037182dadfd3999c0408b289d4
EvgeniaRogotneva <evgeniya.rogotneva@revelsystems.com>
2023-08-18 18:14:15 +0200

    change cones size

--- src/svg_shapes/ConesMarker.js
+++ src/svg_shapes/ConesMarker.js
@@ -4,15 +4,15 @@ const ConesMarker = () => {
       id="cones"
       viewBox="0 0 30 30"
       refX="0"
-      refY="15"
+      refY="8"
       markerWidth="20"
       markerHeight="20"
       orient="auto-start-reverse"
       markerUnits="userSpaceOnUse"
     >
-      <polygon points="0,15 15,15 7,0" fill="purple" />
-      <polygon points="15,15 30,15 22,0" fill="blue" />
-      <polygon points="7.5,30 22.5,30 15,15" fill="orange" />
+      <polygon points="0,12 4,0 8,12" fill="blue" />
+      <polygon points="8,12 12,0 16,12" fill="blue" />
+      <polygon points="4,16 8,2 12,16" fill="orange" />
     </marker>
   );
 };
