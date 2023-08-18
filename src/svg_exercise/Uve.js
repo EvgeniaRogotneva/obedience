a65f0ec2a7b4eca8f4e39f9baf5fd19bd4def4e6
EvgeniaRogotneva <evgeniya.rogotneva@revelsystems.com>
2023-08-18 17:19:09 +0200

    fixed Uve degree

--- src/svg_exercise/Uve.js
+++ src/svg_exercise/Uve.js
@@ -23,13 +23,13 @@ const Uve = ({ metersFromStartToBariers }) => {
   };
 
   const mainLineParam = {
-    mainLineX1: entranceParam["startX"],
+    mainLineX1: entranceParam.startX,
     mainLineX2:
-      entranceParam["startX"] +
-      entranceParam["fromStartToBariiers"] +
+      entranceParam.startX +
+      entranceParam.fromStartToBariiers +
       lenghtFromBarriersToCones,
-    mainLineY1: entranceParam["startY"],
-    mainLineY2: entranceParam["startY"],
+    mainLineY1: entranceParam.startY,
+    mainLineY2: entranceParam.startY,
   };
 
   const StartToBarriersLenght = {
@@ -39,27 +39,26 @@ const Uve = ({ metersFromStartToBariers }) => {
   };
 
   const barriersParam = {
-    x1: entranceParam["startX"] + entranceParam["fromStartToBariiers"],
-    x2: entranceParam["startX"] + entranceParam["fromStartToBariiers"],
-    y1: entranceParam["startY"] - lenghtFromMiddleLineToBarrier,
-    y2: entranceParam["startY"] + lenghtFromMiddleLineToBarrier,
+    x1: entranceParam.startX + entranceParam.fromStartToBariiers,
+    x2: entranceParam.startX + entranceParam.fromStartToBariiers,
+    y1: entranceParam.startY - lenghtFromMiddleLineToBarrier,
+    y2: entranceParam.startY + lenghtFromMiddleLineToBarrier,
   };
 
   const dumbbellParam = {
     x:
-      entranceParam["startX"] +
-      entranceParam["fromStartToBariiers"] +
+      entranceParam.startX +
+      entranceParam.fromStartToBariiers +
       lenghtFromBarriersToDumbbell,
-    firstY1: entranceParam["startY"] - lenghtFromMiddleLineToDumbbell,
+    firstY1: entranceParam.startY - lenghtFromMiddleLineToDumbbell,
     firstY2:
-      entranceParam["startY"] - lenghtFromMiddleLineToDumbbell + lenghtDumbbell,
-    secondY1: entranceParam["startY"] + lenghtFromMiddleLineToDumbbell,
+      entranceParam.startY - lenghtFromMiddleLineToDumbbell + lenghtDumbbell,
+    secondY1: entranceParam.startY + lenghtFromMiddleLineToDumbbell,
     secondY2:
-      entranceParam["startY"] + lenghtFromMiddleLineToDumbbell - lenghtDumbbell,
+      entranceParam.startY + lenghtFromMiddleLineToDumbbell - lenghtDumbbell,
   };
 
-  const rotation = `rotate(${state.Uve.rotation} ,0, ${state.Uve.coord.valueY})`;
-  // const rotation = `rotate(90, 10,10)`;
+  const rotation = `rotate(${state.Uve.rotation} ,${entranceParam.startX}, ${entranceParam.startY})`;
 
   console.log(` rotation ${rotation}`);
 
@@ -73,29 +72,29 @@ const Uve = ({ metersFromStartToBariers }) => {
         <ConesMarker />
       </defs>
       <StartConesLine
-        x1={mainLineParam["mainLineX1"]}
-        x2={mainLineParam["mainLineX2"]}
-        y1={mainLineParam["mainLineY1"]}
-        y2={mainLineParam["mainLineY2"]}
+        x1={mainLineParam.mainLineX1}
+        x2={mainLineParam.mainLineX2}
+        y1={mainLineParam.mainLineY1}
+        y2={mainLineParam.mainLineY2}
         metersFromStartToBariers={
           StartToBarriersLenght[metersFromStartToBariers]
         }
       />
       <BarriersUve
-        x1={barriersParam["x1"]}
-        x2={barriersParam["x2"]}
-        y1={barriersParam["y1"]}
-        y2={barriersParam["y2"]}
+        x1={barriersParam.x1}
+        x2={barriersParam.x2}
+        y1={barriersParam.y1}
+        y2={barriersParam.y2}
       />
       <DumbbellLine
-        x={dumbbellParam["x"]}
-        y1={dumbbellParam["firstY1"]}
-        y2={dumbbellParam["firstY2"]}
+        x={dumbbellParam.x}
+        y1={dumbbellParam.firstY1}
+        y2={dumbbellParam.firstY2}
       />
       <DumbbellLine
-        x={dumbbellParam["x"]}
-        y1={dumbbellParam["secondY1"]}
-        y2={dumbbellParam["secondY2"]}
+        x={dumbbellParam.x}
+        y1={dumbbellParam.secondY1}
+        y2={dumbbellParam.secondY2}
       />
     </g>
   );
