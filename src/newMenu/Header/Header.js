87bd2472c64501501ee5375754f8d28d3f1f8776
EvgeniaRogotneva <evgeniya.rogotneva@revelsystems.com>
2023-09-21 15:59:28 +0200

    add Hide button in header

--- src/newMenu/Header/Header.js
+++ src/newMenu/Header/Header.js
@@ -1,7 +1,9 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
+import Button from "react-bootstrap/esm/Button";
 import Col from "react-bootstrap/esm/Col";
 import Container from "react-bootstrap/esm/Container";
 import Row from "react-bootstrap/esm/Row";
+import { BsArrowDownUp } from "react-icons/bs";
 import { Context } from "../../state/Context";
 import ChangeClassMenu from "../ChangeClassMenu";
 import ChooseClass from "./ChooseClass";
@@ -9,8 +11,9 @@ import FieldSizeSliders from "./FieldSizeSliders";
 
 const Header = () => {
   const [state, dispatch] = useContext(Context);
+  const [hideHeader, setHideHeader] = useState(false);
 
-  return (
+  const response = !hideHeader ? (
     <Container className="Header">
       <Row>
         <Col>
@@ -19,8 +22,31 @@ const Header = () => {
         <Col>
           <FieldSizeSliders />
         </Col>
+        <Col xs={1}>
+          <Button
+            variant="outline-primary"
+            onClick={() => setHideHeader(!hideHeader)}
+          >
+            <BsArrowDownUp />
+          </Button>
+        </Col>
+      </Row>
+    </Container>
+  ) : (
+    <Container className="Header">
+      <Row>
+        <Col>
+          <Button
+            variant="outline-primary"
+            onClick={() => setHideHeader(!hideHeader)}
+          >
+            <BsArrowDownUp />
+          </Button>
+        </Col>
       </Row>
     </Container>
   );
+
+  return response;
 };
 export default Header;
