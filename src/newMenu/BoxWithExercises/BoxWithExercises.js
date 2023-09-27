39db33f63e5f039691677190c94904557848fb0d
EvgeniaRogotneva <evgeniya.rogotneva@revelsystems.com>
2023-09-27 11:08:29 +0200

    add children to BoxWithExercises

--- src/newMenu/BoxWithExercises/BoxWithExercises.js
+++ src/newMenu/BoxWithExercises/BoxWithExercises.js
@@ -1,19 +1,22 @@
 import { useContext } from "react";
 import { Context } from "../../state/Context";
 
-const BoxWithExercises = () => {
+const BoxWithExercises = ({ children }) => {
   const value = useContext(Context);
   const [state] = value;
   const BoxWithExercisesWidth = state.BoxWithExercises.valueX * 10;
   const BoxWithExercisesHeight = state.BoxWithExercises.valueY * 10;
   return (
-    <rect
-      style={{ strokeWidth: 3, stroke: "blue", fill: "white" }}
-      x="0"
-      y="0"
-      width={BoxWithExercisesWidth}
-      height={BoxWithExercisesHeight}
-    />
+    <>
+      <rect
+        style={{ strokeWidth: 1, stroke: "blue", fill: "white" }}
+        x="0"
+        y="0"
+        width={BoxWithExercisesWidth}
+        height={BoxWithExercisesHeight}
+      />
+      {children}
+    </>
   );
 };
 
